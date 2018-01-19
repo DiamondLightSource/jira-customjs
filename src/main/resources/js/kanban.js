08624e29c05b492581a493c2a92e92e124e016d4
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-01-19 09:32:10 +0000

    Add different style sheet for JIRA beta deployment on board 'aaaaa', and make code a bit clearer

--- src/main/resources/js/kanban.js
+++ src/main/resources/js/kanban.js
@@ -1,16 +1,20 @@
 function apply_DLS_custom_css() {
   var boardnumber = /RapidBoard\.jspa\?.*rapidView=([0-9]+)/.exec(window.location.href);
   if (boardnumber) {
-    boardnumber = parseInt(boardnumber[1]);
-    var boardcss = null;
-    if ([1,2007,2019,2027].indexOf(boardnumber) >= 0) {
-      boardcss = 'https://DiamondLightSource.github.io/jira-customjs/test.css';
-    }
-    if (boardcss) {
+    var boardcss = {};
+
+    // board number 1 is only used in the development environment
+    boardcss[1] = 'https://DiamondLightSource.github.io/jira-customjs/scisoftmx.css';
+
+    // Scisoft MX boards
+    boardcss[1677] = boardcss[2007] = boardcss[2019] = boardcss[2027] =
+      'https://DiamondLightSource.github.io/jira-customjs/scisoftmx.css';
+
+    if (boardcss[boardnumber[1]]) {
       var boardcsslink = document.createElement('link');
       boardcsslink.rel = 'stylesheet';
-      boardcsslink.href = boardcss;
-      window.console.log("Applying custom styling from " + boardcss + " for board number " + boardnumber);
+      boardcsslink.href = boardcss[boardnumber[1]];
+      window.console.log("Applying custom styling from " + boardcsslink.href + " for board number " + boardnumber[1]);
       document.head.appendChild(boardcsslink);
     }
   }
