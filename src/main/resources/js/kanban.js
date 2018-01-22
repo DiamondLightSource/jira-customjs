c4880a5395492960e7c6852450c0c0a176f53c88
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-01-22 10:37:13 +0000

    disable styling for demo board 1677 for release

--- src/main/resources/js/kanban.js
+++ src/main/resources/js/kanban.js
@@ -3,11 +3,11 @@ function apply_DLS_custom_css() {
   if (boardnumber) {
     var boardcss = {};
 
-    // board number 1 is only used in the development environment
+    // Board number 1 is only used in the development environment.
     boardcss[1] = 'https://DiamondLightSource.github.io/jira-customjs/scisoftmx.css';
 
     // Scisoft MX boards
-    boardcss[1677] = boardcss[2007] = boardcss[2019] = boardcss[2027] =
+    boardcss[2007] = boardcss[2019] = boardcss[2027] =
       'https://DiamondLightSource.github.io/jira-customjs/scisoftmx.css';
 
     if (boardcss[boardnumber[1]]) {
