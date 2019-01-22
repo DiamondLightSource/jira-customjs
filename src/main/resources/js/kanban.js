96955400877dcca3a792f8e2c6db4a0c1030440a
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-01-22 09:36:40 +0000

    add function to load custom board javascript

--- src/main/resources/js/kanban.js
+++ src/main/resources/js/kanban.js
@@ -1,14 +1,18 @@
-function apply_DLS_custom_css() {
+function apply_DLS_custom_style() {
   var boardnumber = /RapidBoard\.jspa\?.*rapidView=([0-9]+)/.exec(window.location.href);
   if (boardnumber) {
     var boardcss = {};
+    var boardjs = {};
 
     // Board number 1 is only used in the development environment.
     boardcss[1] = 'https://DiamondLightSource.github.io/jira-customjs/scisoftmx.css';
+    boardjs[1] = 'https://DiamondLightSource.github.io/jira-customjs/scisoftmx.js';
 
     // Scisoft MX boards
     boardcss[2007] = boardcss[2026] =
       'https://DiamondLightSource.github.io/jira-customjs/scisoftmx.css';
+    boardjs[2007] = boardjs[2026] =
+      'https://DiamondLightSource.github.io/jira-customjs/scisoftmx.js';
 
     if (boardcss[boardnumber[1]]) {
       var boardcsslink = document.createElement('link');
@@ -16,6 +20,13 @@ function apply_DLS_custom_css() {
       boardcsslink.href = boardcss[boardnumber[1]];
       window.console.log("Applying custom styling from " + boardcsslink.href + " for board number " + boardnumber[1]);
       document.head.appendChild(boardcsslink);
+    }
+    if (boardjs[boardnumber[1]]) {
+      window.console.log("Loading custom javascript from " + boardjs[boardnumber[1]] + " for board number " + boardnumber[1]);
+      $.getScript(boardjs[boardnumber[1]], function(data, textStatus, jqxhr) {
+          window.console.log("Custom javascript loaded from " + boardjs[boardnumber[1]]);
+        }
+      );
     }
 
     // Fix bug of the column group headers appearing in wrong location
@@ -30,4 +41,4 @@ function apply_DLS_custom_css() {
     }, false);
   }
 }
-apply_DLS_custom_css();
+apply_DLS_custom_style();
