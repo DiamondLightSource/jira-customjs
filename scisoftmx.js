b30ab9f5bccd00f38d8b56816d570cb78eb2eedf
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-01-22 14:44:02 +0000

    Add classes for higher priorities

--- scisoftmx.js
+++ scisoftmx.js
@@ -1,4 +1,8 @@
 function DLS_update_issue_css() {
+  $('div.ghx-issue:has(span.ghx-priority[title="High"])').addClass('major-issue');
+  $('div.ghx-issue:has(span.ghx-priority[title="Major"])').addClass('major-issue');
+  $('div.ghx-issue:has(span.ghx-priority[title="Highest"])').addClass('critical-issue');
+  $('div.ghx-issue:has(span.ghx-priority[title="Critical"])').addClass('critical-issue');
   $('div.ghx-issue:has(span.ghx-priority[title="Low"])').addClass('minor-issue');
   $('div.ghx-issue:has(span.ghx-priority[title="Minor"])').addClass('minor-issue');
   $('div.ghx-issue:has(span.ghx-priority[title="Lowest"])').addClass('trivial-issue');
@@ -6,11 +10,9 @@ function DLS_update_issue_css() {
 }
 
 function DLS_tick() {
-  window.console.log("TICK!");
   GH.CallbackManager.registerCallback('gh.work.pool.rendered', 'DLS_event', DLS_tock);
 }
 function DLS_tock() {
-  window.console.log("TOCK!");
   DLS_update_issue_css();
   GH.CallbackManager.registerCallback('gh.work.pool.loaded', 'DLS_event', DLS_tick);
 }
