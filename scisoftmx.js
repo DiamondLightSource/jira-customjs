7e9ec9ca0207f30c6dced358f08b7294859ae655
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-01-22 10:09:15 +0000

    Delay javascript execution until page is loaded

--- scisoftmx.js
+++ scisoftmx.js
@@ -1,4 +1,7 @@
-$('div.ghx-issue:has(span.ghx-priority[title="Low"])').addClass('minor-issue');
-$('div.ghx-issue:has(span.ghx-priority[title="Minor"])').addClass('minor-issue');
-$('div.ghx-issue:has(span.ghx-priority[title="Lowest"])').addClass('minor-issue');
-$('div.ghx-issue:has(span.ghx-priority[title="Trivial"])').addClass('trivial-issue');
+function DLS_update_issue_css() {
+  $('div.ghx-issue:has(span.ghx-priority[title="Low"])').addClass('minor-issue');
+  $('div.ghx-issue:has(span.ghx-priority[title="Minor"])').addClass('minor-issue');
+  $('div.ghx-issue:has(span.ghx-priority[title="Lowest"])').addClass('trivial-issue');
+  $('div.ghx-issue:has(span.ghx-priority[title="Trivial"])').addClass('trivial-issue');
+}
+window.addEventListener('load', DLS_update_issue_css);
