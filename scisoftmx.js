7ed5912505f5ebacabdc33283fd4a747462238a8
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-01-22 14:41:46 +0000

    keep updating priority classes

--- scisoftmx.js
+++ scisoftmx.js
@@ -4,4 +4,21 @@ function DLS_update_issue_css() {
   $('div.ghx-issue:has(span.ghx-priority[title="Lowest"])').addClass('trivial-issue');
   $('div.ghx-issue:has(span.ghx-priority[title="Trivial"])').addClass('trivial-issue');
 }
-window.addEventListener('load', DLS_update_issue_css);
+
+function DLS_tick() {
+  window.console.log("TICK!");
+  GH.CallbackManager.registerCallback('gh.work.pool.rendered', 'DLS_event', DLS_tock);
+}
+function DLS_tock() {
+  window.console.log("TOCK!");
+  DLS_update_issue_css();
+  GH.CallbackManager.registerCallback('gh.work.pool.loaded', 'DLS_event', DLS_tick);
+}
+DLS_tick();
+
+/* Another option:
+ *   AJS.$(document).bind('showLayer', function () {
+ *    ...
+ *   })
+ * but that one is triggered twice
+ */
