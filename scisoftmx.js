7cd1a1747e2fd6575ea9dcbdb1281bee5506b631
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-01-22 09:57:40 +0000

    test board uses JIRA default priority labels

--- scisoftmx.js
+++ scisoftmx.js
@@ -1,2 +1,4 @@
+$('div.ghx-issue:has(span.ghx-priority[title="Low"])').addClass('minor-issue');
 $('div.ghx-issue:has(span.ghx-priority[title="Minor"])').addClass('minor-issue');
+$('div.ghx-issue:has(span.ghx-priority[title="Lowest"])').addClass('minor-issue');
 $('div.ghx-issue:has(span.ghx-priority[title="Trivial"])').addClass('trivial-issue');
